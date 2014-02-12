84ea6b6a15a41a40e1aa9a847bb6c55553235690
Timothy Pratley <timothypratley@gmail.com>
2014-02-11 23:23:43 -0800

    added harmonic sliders

---
+++ web/hi.dart
@@ -0,0 +1,36 @@
+import 'dart:core';
+import 'dart:async';
+import 'dart:html';
+import 'dart:math';
+import 'package:polymer/polymer.dart';
+
+const Tau = PI * 2;
+var es = [querySelector('#s0'),
+          querySelector('#s1'),
+          querySelector('#s2'),
+          querySelector('#s3'),
+          querySelector('#s4'),
+          querySelector('#s5'),
+          querySelector('#s6'),
+          querySelector('#s7'),
+          querySelector('#s8'),
+          querySelector('#s9')];
+
+
+double phi(t, period) {
+  return cos(Tau * (t/period));
+}
+
+double tick = 0.0;
+void handleTimeout(Timer t) {
+  tick += 0.001;
+  for (var i=0; i<10; i++) {
+    var v = 50*(1+phi(tick, 1/(i+46)));
+    es[i].value = v.toString();
+  }
+}
+
+void main() {
+  initPolymer();
+  new Timer.periodic(new Duration(milliseconds: 30), handleTimeout);
+}
